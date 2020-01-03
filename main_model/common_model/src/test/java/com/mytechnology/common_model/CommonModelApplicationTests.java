1e96156d419e4fbcdf726058850eec76e7fc5414
yulongshuai878 <41602302+yulongshuai878@users.noreply.github.com>
2020-01-03 14:58:25 +0800

    init project
    init project

---
+++ main_model/common_model/src/test/java/com/mytechnology/common_model/CommonModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytechnology.common_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
